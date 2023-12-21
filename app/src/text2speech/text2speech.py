28ddb364d3aa24856ca8370a843114d2aa7ffc4d
Srujan-rai <raisrujan@gmail.com>
2023-12-21 23:12:12 +0530

    code changes

--- app/src/text2speech/text2speech.py
+++ app/src/text2speech/text2speech.py
@@ -1,13 +1,8 @@
 import speech_recognition as sr
-
-
 recognizer = sr.Recognizer()
-
-
 with sr.Microphone() as source:
     print("Say something...")
     audio = recognizer.listen(source)
-
 try:
     text = recognizer.recognize_google(audio)
     print("TEXT:", text)
