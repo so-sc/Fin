5c5ae22560a72bf9a04e65a13dc3db2b57392b53
Srujan-rai <raisrujan@gmail.com>
2023-12-21 23:31:26 +0530

    code changes

---
+++ web/api/src/ml/text2speech.py
@@ -0,0 +1,12 @@
+import speech_recognition as sr
+recognizer = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something...")
+    audio = recognizer.listen(source)
+try:
+    text = recognizer.recognize_google(audio)
+    print("TEXT:", text)
+except sr.UnknownValueError:
+    print("could not understand audio.")
+except sr.RequestError as e:
+    print("Error fetching results; {0}".format(e))
