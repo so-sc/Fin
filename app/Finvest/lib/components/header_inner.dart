e9e2be1c2b14a2070664a333454c05031c6132e9
heimanpictures <heimanpicturesofficial@gmail.com>
2023-12-22 03:15:05 +0530

    Setting up app

---
+++ app/Finvest/lib/components/header_inner.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/widgets.dart';
+import 'const.dart';
+
+class HeaderInner extends StatelessWidget {
+  const HeaderInner({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Expanded(
+          flex: 1,
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.vertical(bottom: Radius.circular(50.0)),
+              color: Constants.blueLight,
+            ),
+            child: Stack(
+              children: <Widget>[
+                Positioned(
+                  top: -100,
+                  right: -130,
+                  child: Image.asset("assets/images/blob_2.png",
+                      width: 352,
+                      height: 343,
+                      color: Constants.blueDark),
+                ),
+                Positioned(
+                  top: 20,
+                  left: -100,
+                  child: Image.asset("assets/images/blob_1.png",
+                      width: 302,
+                      height: 343,
+                      color: Constants.blueMain),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Expanded(
+            flex: 1,
+            child: Container()
+        )
+      ],
+    );
+  }
+}
--- app/Finvest/lib/pages/splash.dart
+++ app/Finvest/lib/pages/splash.dart
@@ -1,10 +1,36 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+import 'onboard/onboarding.dart';
+
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const OnBoardingScreen()),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Your Splash Screen Content',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
   }
 }
